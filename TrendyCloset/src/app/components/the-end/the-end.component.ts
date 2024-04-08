1a5cf9ca287abd22788f379b8a9a3b636455e506
DrCetmi <drcetmi@gmail.com>
2024-04-08 09:57:33 +0200

    the end page added

---
+++ TrendyCloset/src/app/components/the-end/the-end.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-the-end',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './the-end.component.html',
+  styleUrls: ['./the-end.component.scss'],
+})
+export class TheEndComponent {
+  
+  
+
+}
